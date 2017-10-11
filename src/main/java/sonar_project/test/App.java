db8ccf15e078aec7f14de93da9086ada3bb005e8
Pedro Pires Pereira <pedro.pires_pereira@siemens.com>
2017-10-11 15:43:20 +0100

    first commit

---
+++ src/main/java/sonar_project/test/App.java
@@ -0,0 +1,25 @@
+package sonar_project.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println( "Hello World!" );
+       System.out.println( "Hello World!" );
+       System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "HOPE, WE NEED HOPE!" );
+        System.out.println( "HOPE, WE NEED HOPE!" );
+         System.out.println( "HOPE, WE NEED HOPE!" );
+         System.out.println( "HOPE, WE NEED HOPE!" );
+         System.out.println( "HOPE, WE NEED HOPE!" );
+        
+    }
+}
